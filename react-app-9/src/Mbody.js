d149e96e7036ef37f6c85fe8bffbcc2eb82cc368
bug-dor <tolyaumer@rambler.ru>
2020-04-03 12:16:02 +0300

    fixed Mbody -> add event.target input

--- react-app-9/src/Mbody.js
+++ react-app-9/src/Mbody.js
@@ -1,24 +1,26 @@
 import React from 'react'
 
+
 class Mbody extends React.Component {
    constructor() {
        super()
        this.state={
-           names: ['Kol','Yan','Sob','Pussy']
+        value: 'petuh'
        }
+       this.handleChange = this.handleChange.bind(this)
    }
-
+   handleChange(event) {
+       console.log('OPa');
+       this.setState({value: event.target.value})
+   }
    render() {
-       const Names = this.state.names.map((name,index) => {
-           return <li key={index}>{name}</li>
-       });
-    
+       
+       
        return (
-           
-               <ul>
-                   {Names}
-               </ul>
-           
+            <div>
+               
+               <input value={this.state.value} onChange={this.handleChange}></input>
+            </div>
        )
    }
 }
