284b26e03165912c85012067ff282e81762268ab
bug-dor <tolyaumer@rambler.ru>
2020-04-03 13:51:25 +0300

    added form

--- react-app-9/src/Mbody.js
+++ react-app-9/src/Mbody.js
@@ -5,23 +5,38 @@ class Mbody extends React.Component {
    constructor() {
        super()
        this.state={
-        value: 'petuh'
+           items:[1,2,3,4,5],
+           value:''
        }
        this.handleChange = this.handleChange.bind(this)
+       this.addItem = this.addItem.bind(this)
+
+   }
+   addItem(event) {
+       this.state.items.push(this.state.value)
+       this.setState({items: this.state.items})
+       event.preventDefault()
    }
    handleChange(event) {
-       console.log('OPa');
        this.setState({value: event.target.value})
    }
    render() {
-       
+       const list = this.state.items.map((item,index) =>{
+         return <li key={index}>{item}</li>
+       })
        
        return (
-            <div>
-               
-               <input value={this.state.value} onChange={this.handleChange}></input>
-            </div>
+           <div>
+               <ul>
+                   {list}
+               </ul>
+              <form onSubmit={this.addItem}>
+                 <input value={this.state.value} onChange={this.handleChange} />
+                 <input type="submit" />
+              </form>
+           </div>
        )
    }
+
 }
 export default Mbody
